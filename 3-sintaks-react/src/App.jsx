f105f5fb4813edeeeb2bb57f7a6b08b805a30dc1
ehanz12 <rhanss@gmail.com>
2025-04-13 09:16:11 +0700

    Modip buaat fe

---
+++ 3-sintaks-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Default route for Login */}
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ mapulasi dom/script.js
@@ -0,0 +1,5 @@
+const app = document.getElementsByid('app');
+const header = document.createElement('h1');
+const headercontent = document.createTextNode('belajar react di WPU:)');
+header.appendChild(headercontent);
+app.appendChild(header);
